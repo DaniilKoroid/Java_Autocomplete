1d4fa63aa88106b27a97c861a3c42b9319bf3c88
Daniil Koroid <Daniil_Koroid@epam.com>
2016-03-16 19:09:22 +0200

    Changed wordsWithPrefix(String, int) method.

--- src/main/java/ua/daniilkoroid/autocomplete/PrefixMatches.java
+++ src/main/java/ua/daniilkoroid/autocomplete/PrefixMatches.java
@@ -20,153 +20,148 @@ import ua.daniilkoroid.autocomplete.trie.Tuple;
  */
 public class PrefixMatches {
 
-    /**
-     * Default length of search.
-     */
-    private final int DEFAULT_SEARCH_LENGTH = 3;
-    
-    /**
-     * Minimal length of string that can be added to in-memory dictionary.
-     */
-    private final int MINIMAL_STRING_TO_ADD_LENGTH = 3;
+	/**
+	 * Default length of search.
+	 */
+	private final int DEFAULT_SEARCH_LENGTH = 3;
 
-    /**
-     * Trie that is used to store words.
-     */
-    private final Trie trie;
+	/**
+	 * Minimal length of string that can be added to in-memory dictionary.
+	 */
+	private final int MINIMAL_STRING_TO_ADD_LENGTH = 3;
 
-    /**
-     * Create object with default in-memory dictionary which is
-     * {@link RWayTrie}.
-     */
-    public PrefixMatches() {
-        trie = new RWayTrie();
-    }
+	/**
+	 * Trie that is used to store words.
+	 */
+	private final Trie trie;
 
-    /**
-     * Create object with given in-memory dictionary.
-     *
-     * @param trie in-memory dictionary to be used
-     */
-    public PrefixMatches(Trie trie) {
-        this.trie = trie;
-    }
+	/**
+	 * Create object with default in-memory dictionary which is {@link RWayTrie}
+	 * .
+	 */
+	public PrefixMatches() {
+		trie = new RWayTrie();
+	}
 
-    /**
-     * Add given strings to in-memory dictionary.
-     *
-     * @param strings strings to be stored in in-memory dictionary
-     * @return number of strings that were added
-     */
-    public int add(String... strings) {
-        int beforeAddSize = size();
-        for (String string : strings) {
-            if(string.length() < MINIMAL_STRING_TO_ADD_LENGTH) {
-                continue;
-            }
-            trie.add(new Tuple(string));
-        }
-        int afterAddSize = size();
-        int addedElements = afterAddSize - beforeAddSize;
-        return addedElements;
-    }
+	/**
+	 * Create object with given in-memory dictionary.
+	 *
+	 * @param trie
+	 *            in-memory dictionary to be used
+	 */
+	public PrefixMatches(Trie trie) {
+		this.trie = trie;
+	}
 
-    /**
-     * Checks if given word is contained.
-     *
-     * @param word word to check for being contained
-     * @return <code>true</code> if word is contained. Otherwise -
-     * <code>false</code>
-     */
-    public boolean contains(String word) {
-        return trie.contains(word);
-    }
+	/**
+	 * Add given strings to in-memory dictionary.
+	 *
+	 * @param strings
+	 *            strings to be stored in in-memory dictionary
+	 * @return number of strings that were added
+	 */
+	public int add(String... strings) {
+		int beforeAddSize = size();
+		for (String string : strings) {
+			if (string.length() < MINIMAL_STRING_TO_ADD_LENGTH) {
+				continue;
+			}
+			trie.add(new Tuple(string));
+		}
+		int afterAddSize = size();
+		int addedElements = afterAddSize - beforeAddSize;
+		return addedElements;
+	}
 
-    /**
-     * Deletes given word if such is contained.
-     * <p>
-     * If given word is present - deletes it and returns <code>true</code>. If
-     * given word is not present - does nothing and returns <code>false</code>.
-     * </p>
-     *
-     * @param word word to delete
-     * @return <code>true</code> if word is contained and was deleted. Otherwise
-     * - <code>false</code>
-     */
-    public boolean delete(String word) {
-        return trie.delete(word);
-    }
+	/**
+	 * Checks if given word is contained.
+	 *
+	 * @param word
+	 *            word to check for being contained
+	 * @return <code>true</code> if word is contained. Otherwise -
+	 *         <code>false</code>
+	 */
+	public boolean contains(String word) {
+		return trie.contains(word);
+	}
 
-    /**
-     * Get stored words count.
-     *
-     * @return stored words count
-     */
-    public int size() {
-        return trie.size();
-    }
+	/**
+	 * Deletes given word if such is contained.
+	 * <p>
+	 * If given word is present - deletes it and returns <code>true</code>. If
+	 * given word is not present - does nothing and returns <code>false</code>.
+	 * </p>
+	 *
+	 * @param word
+	 *            word to delete
+	 * @return <code>true</code> if word is contained and was deleted. Otherwise
+	 *         - <code>false</code>
+	 */
+	public boolean delete(String word) {
+		return trie.delete(word);
+	}
 
-    /**
-     * Iterator over all words that start from given prefix and have length that
-     * is not longer than given prefix size and given parameter k.
-     *
-     * @param pref prefix for all words to find
-     * @param k maximum differ length from prefix length
-     * @return iterator over all words that start from given prefix and have
-     * length that is not longer than given prefix size and given parameter k.
-     */
-    public Iterable<String> wordsWithPrefix(String pref, int k) {
-        Iterable<String> wordsWithPrefix = trie.wordsWithPrefix(pref);
-        LinkedList<String> wordsWithPrefixAndLength = new LinkedList<String>();
-        int prefLength = pref.length();
-        for (int curLength = prefLength; curLength < prefLength + k; curLength++) {
-            Iterable<String> wordsOfGivenLength = getWordsOfGivenLength(wordsWithPrefix, curLength);
-            wordsWithPrefixAndLength.addAll((Collection<? extends String>) wordsOfGivenLength);
-        }
-        return wordsWithPrefixAndLength;
-    }
+	/**
+	 * Get stored words count.
+	 *
+	 * @return stored words count
+	 */
+	public int size() {
+		return trie.size();
+	}
 
-    /**
-     * Iterator over all words that start from given prefix and have length that
-     * is not longer than given prefix size and
-     * {@link PrefixMatches#DEFAULT_SEARCH_LENGTH}.
-     *
-     * @param pref prefix for all words to find
-     * @return iterator over all words that start from given prefix and have
-     * length that is not longer than given prefix size and given parameter
-     * {@link PrefixMatches#DEFAULT_SEARCH_LENGTH}.
-     */
-    public Iterable<String> wordsWithPrefix(String pref) {
-        return wordsWithPrefix(pref, DEFAULT_SEARCH_LENGTH);
-    }
+	/**
+	 * Iterable over all words that start from given prefix and have different
+	 * lengths count that is not greater than given parameter k.
+	 * <p>
+	 * If no such words are found - {@link Collections#emptyList()} is returned.
+	 * </p>
+	 * 
+	 * @param pref
+	 *            prefix for all words to find
+	 * @param k
+	 *            maximum differ lengths count
+	 * @return iterable over all words that start from given prefix and
+	 *         different lengths count that is not greater than given parameter
+	 *         k. If no such words are found - {@link Collections#emptyList()}
+	 *         is returned
+	 */
+	public Iterable<String> wordsWithPrefix(String pref, int k) {
+		Iterable<String> wordsWithPrefix = trie.wordsWithPrefix(pref);
+		LinkedList<String> wordsWithPrefixAndLength = new LinkedList<String>();
+		int prefLength = pref.length();
+		int currentPrefLength = prefLength;
+		int differentLengthCounter = 0;
+		for (String wordWithPrefix : wordsWithPrefix) {
+			if (wordWithPrefix.length() != currentPrefLength) {
+				differentLengthCounter++;
+				if (differentLengthCounter >= k) {
+					break;
+				}
+			}
+			wordsWithPrefixAndLength.add(wordWithPrefix);
+		}
+		Iterable<String> result;
+		if (wordsWithPrefixAndLength.isEmpty()) {
+			result = Collections.emptyList();
+		} else {
+			result = wordsWithPrefixAndLength;
+		}
+		return result;
+	}
 
-    /**
-     * Get words from given {@link Iterable} with length that is given as
-     * parameter.
-     * <p>
-     * If there are no words with such length - returns
-     * {@link Collections#emptyList()}
-     * </p>
-     *
-     * @param iterable iterable to find words of given length
-     * @param length length of words to add to returned iterable
-     * @return {@link Iterable} with words of given length if found such.
-     * Otherwise - returns {@link Collections#emptyList()}
-     */
-    private Iterable<String> getWordsOfGivenLength(Iterable<String> iterable, int length) {
-    	Iterable<String> result;
-        LinkedList<String> wordsOfGivenLength;
-        wordsOfGivenLength = new LinkedList<String>();
-        for (String word : iterable) {
-            if (word.length() == length) {
-                wordsOfGivenLength.add(word);
-            }
-        }
-        if(wordsOfGivenLength.isEmpty()) {
-        	result = Collections.emptyList();
-        } else {
-        	result = wordsOfGivenLength;
-        }
-        return result;
-    }
+	/**
+	 * Iterator over all words that start from given prefix and have length that
+	 * is not longer than given prefix size and
+	 * {@link PrefixMatches#DEFAULT_SEARCH_LENGTH}.
+	 *
+	 * @param pref
+	 *            prefix for all words to find
+	 * @return iterator over all words that start from given prefix and have
+	 *         length that is not longer than given prefix size and given
+	 *         parameter {@link PrefixMatches#DEFAULT_SEARCH_LENGTH}.
+	 */
+	public Iterable<String> wordsWithPrefix(String pref) {
+		return wordsWithPrefix(pref, DEFAULT_SEARCH_LENGTH);
+	}
 }
